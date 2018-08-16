0ccfb0011a63452650d11bc020417a098c80d73b
Mathieu Plourde <mat.plourde@gmail.com>
2018-08-15 23:08:50 -0400

    Add xunit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Xunit;
 
 namespace test_cicd
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2,2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2,2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+        /*static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-        }
+        }*/
     }
 }
